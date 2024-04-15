c88f8687e5609afd5b33388609cc2852cee0c65b
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-15 12:34:06 +0200

    Přidána funkcionalita hry s bonusovými úkoly.

--- hra.js
+++ hra.js
@@ -1,4 +1,7 @@
+import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
+
 let currentPlayer = "circle";
+const gameBoxes = document.querySelectorAll(".game__box");
 
 const clicking = (event) => {
   const btn = event.target;
@@ -9,32 +12,47 @@ const clicking = (event) => {
     currentPlayer = "cross";
     playerSymbol.setAttribute("src", "cross.svg");
     playerSymbol.setAttribute("alt", "křížek");
-    
   } else if (currentPlayer === "cross") {
     btn.classList.add("game__field--cross");
     currentPlayer = "circle";
     playerSymbol.setAttribute("src", "circle.svg");
     playerSymbol.setAttribute("alt", "kolečko");
   }
+
+  const gameField = [...gameBoxes].map((item) => {
+    if (item.classList.contains("game__field--circle")) {
+      return "o";
+    } else if (item.classList.contains("game__field--cross")) {
+      return "x";
+    } else {
+      return "_";
+    }
+  });
+
+  const returningWinner = () => {
+    const winner = findWinner(gameField);
+    if (winner === "o" || winner === "x") {
+      alert(`Vyhrál hráč se symbolem "${winner.toUpperCase()}"!`);
+      location.reload();
+    } else if (winner === "tie") {
+      alert("Hra skončila nerozhodně.");
+    }
+  };
+
+  setTimeout(returningWinner, 500);
 };
 
 const leaveConfirm = (event) => {
-    const confirmation = confirm("Opravdu chceš začít znovu?");
-    if (confirmation !== true) {
-        event.preventDefault();
-    }
+  const confirmation = confirm("Opravdu chceš začít znovu?");
+  if (confirmation !== true) {
+    event.preventDefault();
+  }
 };
 
-document.querySelector("button:nth-child(1)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(2)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(3)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(4)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(5)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(6)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(7)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(8)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(9)").addEventListener("click", clicking);
-document.querySelector("button:nth-child(10)").addEventListener("click", clicking);
+gameBoxes.forEach((box) => {
+  box.addEventListener("click", clicking);
+});
 
-
-document.querySelector(".game__link--restart").addEventListener("click", leaveConfirm);
+document
+  .querySelector(".game__link--restart")
+  .addEventListener("click", leaveConfirm);
